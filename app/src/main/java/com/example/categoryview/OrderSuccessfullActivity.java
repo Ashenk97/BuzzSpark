cc39026f9d804e141572b0a522d84bb5bf23d576
Navoda <it18108132@my.sliit.lk>
2019-09-22 18:34:09 +0530

    All clear cart

---
+++ app/src/main/java/com/example/categoryview/OrderSuccessfullActivity.java
@@ -0,0 +1,14 @@
+package com.example.categoryview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderSuccessfullActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_successfull);
+    }
+}
